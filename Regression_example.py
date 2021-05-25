1d16e7278e3c30f6fc7dbd0f94ade7032b404ecb
Jongwon park <73830789+jjongdals@users.noreply.github.com>
2021-05-25 23:36:05 +0900

    Add files via upload
    bostson_housing example

---
+++ Regression_example.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from tensorflow.keras.datasets import boston_housing
+(train_X, train_Y), (test_X, test_Y) = boston_housing.load_data()
+
+print(len(train_X), len(test_X))
+print(train_X[0])
+print(train_Y[0])
+
+
+# In[3]:
+
+
+x_mean = train_X.mean(axis=0)
+x_std = train_X.std(axis=0)
+
+train_X -= x_mean
+train_X /= x_std
+test_X -= x_mean
+test_X /= x_std
+
+y_mean = train_Y.mean(axis=0)
+y_std = train_Y.std(axis=0)
+
+train_Y -= y_mean
+train_Y /= y_std
+test_Y -= y_mean
+test_Y /= y_std
+
+print(train_X[0])
+print(train_Y[0])
+
+
+# In[11]:
+
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(units=65, activation='relu', input_shape=(13, )),
+    tf.keras.layers.Dense(units=52, activation='relu'),
+    tf.keras.layers.Dense(units=39, activation='relu'),
+    tf.keras.layers.Dense(units=26, activation='relu'),
+    tf.keras.layers.Dense(units=1)
+])
+
+model.compile(optimizer=tf.keras.optimizers.Adam(lr=0.06), loss='mse')
+history = model.fit(train_X, train_Y, epochs = 30, batch_size =32, validation_split=0.25,
+callbacks=[tf.keras.callbacks.EarlyStopping(patience=3, monitor='val_lass')])
+
+plt.plot(history.history['loss'], 'b-', label='loss')
+plt.plot(history.history['val_loss'], 'r--', label='val_loss')
+plt.xlabel('Epoch')
+plt.legend()
+plt.show()
+
+model.evaluate(test_X, test_Y)
+
+
+# In[12]:
+
+
+pred_Y = model.predict(test_X)
+
+plt.figure(figsize=(5, 5))
+plt.plot(test_Y, pred_Y, 'r.')
+plt.axis([min(test_Y), max(test_Y), min(test_Y), max(test_Y)])
+
+plt.plot([min(test_Y), max(test_Y)], [min(test_Y), max(test_Y)], ls='--', c=".3")
+plt.xlabel('test_Y')
+plt.ylabel('pred_Y')
+
+plt.show()
+
+
+# In[ ]:
+
+
+
+
