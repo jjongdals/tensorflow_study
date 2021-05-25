fc3e468343cbd116de3a2a96cff6ec6bca27cf26
Jongwon park <73830789+jjongdals@users.noreply.github.com>
2021-05-25 23:39:32 +0900

    Update Regression_example.py

--- Regression_example.py
+++ Regression_example.py
@@ -1,19 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[2]:
-
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
 from tensorflow.keras.datasets import boston_housing
 (train_X, train_Y), (test_X, test_Y) = boston_housing.load_data()
-
-print(len(train_X), len(test_X))
-print(train_X[0])
-print(train_Y[0])
-
-
-# In[3]:
-
 
 x_mean = train_X.mean(axis=0)
 x_std = train_X.std(axis=0)
@@ -31,16 +22,7 @@ train_Y /= y_std
 test_Y -= y_mean
 test_Y /= y_std
 
-print(train_X[0])
-print(train_Y[0])
-
-
-# In[11]:
-
-
-import tensorflow as tf
-import matplotlib.pyplot as plt
-
+#model
 model = tf.keras.Sequential([
     tf.keras.layers.Dense(units=65, activation='relu', input_shape=(13, )),
     tf.keras.layers.Dense(units=52, activation='relu'),
@@ -59,10 +41,8 @@ plt.xlabel('Epoch')
 plt.legend()
 plt.show()
 
+#regression model evaluate
 model.evaluate(test_X, test_Y)
-
-
-# In[12]:
 
 
 pred_Y = model.predict(test_X)
@@ -76,10 +56,3 @@ plt.xlabel('test_Y')
 plt.ylabel('pred_Y')
 
 plt.show()
-
-
-# In[ ]:
-
-
-
-
