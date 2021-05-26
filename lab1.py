dc5f5ef7647657eb8b418603779ca2e687bb83bc
Jongwon park <73830789+jjongdals@users.noreply.github.com>
2021-05-26 22:09:12 +0900

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,10 +1,15 @@
-import numpy as np import tensorflow as tf import math
+import numpy as np 
+import tensorflow as tf 
+import math
 
-def sigmoid(x): return 1 / 1 + exp(-x)
+def sigmoid(x): 
+  return 1 / 1 + exp(-x)
 
-x = np.array([[1,1], [1,0], [0,1], [0,0]]) y = np.array([[0], [1], [1], [0]])
+x = np.array([[1,1], [1,0], [0,1], [0,0]]) 
+y = np.array([[0], [1], [1], [0]])
 
-model = tf.keras.Sequential([ tf.keras.layers.Dense(units=2, activation = 'sigmoid', input_shape = (2,)), tf.keras.layers.Dense(units=1, activation = 'sigmoid') ])
+model = tf.keras.Sequential([ tf.keras.layers.Dense(units=2, activation = 'sigmoid', input_shape = (2,)), 
+tf.keras.layers.Dense(units=1, activation = 'sigmoid') ])
 
 model.compile(optimizer=tf.keras.optimizers.SGD(lr=0.1), loss='mse')
 
